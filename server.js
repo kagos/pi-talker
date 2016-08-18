670ef6d85c61b2291882485e276f0e29805c2751
kagos <kristin.agos@yahoo.com>
2016-08-18 12:15:41 -0500

    Toggle and response from client to run Python script

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
+const router = express.Router();
+
+const lightOn = () => {
+  console.log("LightOn");
+
+  PythonShell.run('python/lighton.py', function (err) {
+    if (err) return err;
+
+    console.log('lighton.py finished');
+
+    return "OK";
+  });
+};
+
+const lightOff = () => {
+  console.log("LightOff");
+
+  PythonShell.run('python/lightoff.py', function (err) {
+   if (err) return err;
+
+   console.log('lightoff.py finished');
+
+   return "OK";
+  });
+};
 
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
@@ -10,21 +35,21 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.use(express.static(__dirname + '/src'));
 
-app.get('*', function(req, res) {
-  res.sendfile('src/index.html');
-});
+app.get('/',
+  function(req, res) {
+    res.sendfile('src/index.html');
+  }
+).post('/',
+  function(req, res) {
 
+    const status = req.body.light.status;
+    let response = status == 1 ? lightOn() || "OK - On" : lightOff() || "OK - Off";
+
+    res.json({success: true, data: {pyResponse: response}});
+  }
+);
+
+app.use('/api', router);
 app.listen(PORT);
 
 console.log('Server started on ' + PORT);
-
-PythonShell.run('python/lighton.py', function (err) {
-  if (err) throw err;
-  console.log('lighton.py finished');
-});
-
-
-PythonShell.run('python/lightoff.py', function (err) {
- if (err) throw err;
- console.log('lightoff.py finished');
-});
