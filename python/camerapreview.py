208a30b9e8ba2fb6b0389b33b740856b332354ec
kagos <kristin.agos@yahoo.com>
2016-08-22 23:48:21 -0500

    added camera preview, returns JSON of filename in local fs

---
+++ python/camerapreview.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+GPIO.setmode(GPIO.BCM)
+ 
+#Camera Options
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+camera.vflip = True
+ 
+#The LED
+led = 16
+GPIO.setup(led, GPIO.OUT)
+
+
+GPIO.output(led, 1)# Switch on the LED
+camera.start_preview() #Start Camera
+sleep(2) #CameraWarmup
+currenttime=str(datetime.now()) # Get current time
+filename = '%s.jpg' % currenttime # Create timestamped filename
+camera.capture(filename) # write file
+print ('file', filename)
+GPIO.output(led, 0) # Close LED
+
+GPIO.cleanup() #cleanup GPIO hooks in kernel
