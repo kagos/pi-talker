fb79102ce1fcbcece30294cdbae1a110809c5b2a
kagos <kristin.agos@yahoo.com>
2016-08-22 23:41:21 -0500

    added features to buttonpush.py includes KeyboardInterrupt catch and filename corrections

--- python/buttonpush.py
+++ python/buttonpush.py
@@ -1,22 +1,39 @@
 import RPi.GPIO as GPIO
-import time
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
 GPIO.setmode(GPIO.BCM)
  
 # Pin 19 will sense for button pushing
 button = 24
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+#Camera Options
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+camera.vflip = True
  
-# The LED
+#The LED
 led = 16
 GPIO.setup(led, GPIO.OUT)
- 
-while True:
-    input_state = GPIO.input(button) # Sense the button
-    if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.2)
-        # Switch on LED
-        #GPIO.output(led, 1)
-    else :
-        # Switch off LED
-        GPIO.output(led, 0)
+try: 
+	while True:
+	    input_state = GPIO.input(button) # Sense the button
+	    if input_state == False: # Backwards logic for on-board pull-down resistor
+	    	print('Grabbing frame')
+     	    	GPIO.output(led, 1)# Switch on the LED
+	    	camera.start_preview() #Start Camera
+	    	sleep(2) #CameraWarmup
+	    	currenttime=str(datetime.now()) # Get current time
+	    	filename = '%s.jpg' % currenttime # Create timestamped filename
+	    	camera.capture(filename) # write file
+	    	print ('frame written as', filename)
+	    	GPIO.output(led, 0) # Close LED
+
+  	else:
+           	# Switch off LED
+           	GPIO.output(led, 0)
+except KeyboardInterrupt:
+	print ('\nClosing GPIO')
+	GPIO.cleanup()
